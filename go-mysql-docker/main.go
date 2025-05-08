325d5990a7b390910f6ee47ea3cd1aa6bb6adc68
tran.thanh.son <tran.thanh.son@sun-asterisk.com>
2025-05-08 15:21:52 +0700

    lession 2 basic code full

--- go-mysql-docker/main.go
+++ go-mysql-docker/main.go
@@ -6,9 +6,36 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"runtime"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+func printGoVersion() {
+	fmt.Println("Go version:", runtime.Version())
+}
+
+func printValue[T any](value T) {
+	fmt.Printf("Value: %v\n", value)
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) greet() string {
+	return fmt.Sprintf("Hello, my name is %s and I am %d years old.", p.Name, p.Age)
+}
+
+func checkAge(age int) string {
+    if age < 18 {
+        return "Underage"
+    } else if age >= 18 && age <= 65 {
+        return "Adult"
+    }
+    return "Senior"
+}
 
 func main() {
 	dsn := "root:password@tcp(db:3306)/testdb"
@@ -32,7 +59,15 @@ func main() {
 	fmt.Println("Mysql connection established!")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Server is running!")
+		fmt.Fprintf(w, "Server is running!\n")
+
+		printGoVersion()
+		printValue(42)
+		printValue("Hello!")
+	
+		p := Person{Name: "Son", Age: 31}
+		fmt.Fprintf(w, p.greet() + "\n")
+		fmt.Fprintf(w, checkAge(p.Age) + "\n")
 	})
 
 	fmt.Println("HTTP server is running on port 8081...")
